c74e9bb885811d6b938510bda6105abb09325bab
vuoso-l <vuosolucas@gmail.com>
2021-12-11 10:20:25 -0300

    Modified config

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,8 +2,8 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
     cloud_name: "hjcssyqov",
-    api_key: CLOUD_API_KEY,
-    api_secret: CLOUD_API_SECRET,
+    api_key: 433589858897849,
+    api_secret: DEwzR6PanrT19W88Kho3BL8dmDk,
 });
 
 module.exports = cloudinary;
