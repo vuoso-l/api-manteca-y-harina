89fc9cedfc41a5e8d74c1caac6ab0912000a1dc2
vuoso-l <vuosolucas@gmail.com>
2021-12-16 18:27:53 -0300

    Delete getUploadRoute

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -3,10 +3,8 @@ const router = express.Router();
 
 //Se importa los controladores
 const uploadController = require("../controllers/upload/uploadController");
-const getUploadController = require("../controllers/upload/getUploadController");
 
 //Rutas para la subida de imágenes
-router.get("/upload", getUploadController.getUpload);
 router.post("/upload", uploadController.postUpload);
 
 module.exports = router;
